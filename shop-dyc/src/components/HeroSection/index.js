08919a1abf6f489efba6bc0af4c12b42b72f6860
SentientBagel <jhnelson.contact@gmail.com>
2023-01-20 04:18:55 -0500

    starting hero section

---
+++ shop-dyc/src/components/HeroSection/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <HeroContainer>
+      <HeroBg>
+        
+      </HeroBg>
+    </HeroContainer>
+  )
+}
+
+export default HeroSection
